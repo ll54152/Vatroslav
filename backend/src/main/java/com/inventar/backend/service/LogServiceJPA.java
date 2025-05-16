390558bfa6d4f46beda078d1f9b0fa7943b7056c
Luka Lulic <ll54152@fer.hr>
2025-05-16 22:03:58 +0200

    Added various functions to LogServiceJPA

--- backend/src/main/java/com/inventar/backend/service/LogServiceJPA.java
+++ backend/src/main/java/com/inventar/backend/service/LogServiceJPA.java
@@ -5,6 +5,8 @@ import com.inventar.backend.repo.*;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.stereotype.*;
 
+import java.util.List;
+
 @Service
 public class LogServiceJPA {
 
@@ -13,5 +15,27 @@ public class LogServiceJPA {
 
     public Log save(Log log) {
         return logRepo.save(log);
+    }
+
+    public List<Log> findAll() {
+        return logRepo.findAll();
+    }
+
+    public Log findById(Long id) {
+        return logRepo.findById(id).orElse(null);
+    }
+
+    public List<Log> findByEksperimentId(Long eksperimentId) {
+        List<Log> logs = logRepo.findAll();
+        return logs.stream()
+                .filter(log -> log.getEksperiment().getId().equals(eksperimentId))
+                .toList();
+    }
+
+    public List<Log> findByKomponentaId(int komponentaId) {
+        List<Log> logs = logRepo.findAll();
+        return logs.stream()
+                .filter(log -> log.getKomponenta().getId().equals(komponentaId))
+                .toList();
     }
 }
