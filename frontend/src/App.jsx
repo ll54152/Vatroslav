6f78b72abceba4936c9033f29877adc298384608
Luka Lulic <ll54152@fer.hr>
2025-05-21 21:00:26 +0200

    Revert "Added BrowserRouter"
    This reverts commit 6af61c6d405eada646772684e8d5d6ea4626897b.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useNavigate} from 'react-router-dom';
 import Home from './pages/Home';
 import Experimentunos from './pages/Experimentunos';
 import Login from './pages/Login';
@@ -12,22 +12,29 @@ import Komponenteunos from './pages/Komponenteunos';
 import Komponente from './pages/Komponente';
 
 function App() {
-    return (
-        <Routes>
-            <Route index element={<Home />} />
-            <Route path="/Home" element={<Home />} />
-            <Route path="/Login" element={<Login />} />
-            <Route path="/Signup" element={<Signup />} />
-            <Route path="/MainPage" element={<MainPage />} />
-            <Route path="/Experimenti" element={<Experimenti />} />
-            <Route path="/experimentiprimjer/:id" element={<Experimentiprimjer />} />
-            <Route path="/experimenti/edit/:id" element={<EksperimentEdit />} />
-            <Route path="/komponenteprimjer/:id" element={<Komponenteprimjer />} />
-            <Route path="/Experimentunos" element={<Experimentunos />} />
-            <Route path="/Komponenteunos" element={<Komponenteunos />} />
-            <Route path="/Komponente" element={<Komponente />} />
-        </Routes>
-    );
+
+  return (
+    <>
+     <BrowserRouter>
+      <Routes>
+        <Route index element={<Home/>}/>
+        <Route path="/Home" element={<Home/>} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Signup" element={<Signup />} />
+        <Route path="/MainPage" element={<MainPage />} />
+        <Route path="/Experimenti" element={<Experimenti />} />
+        <Route path="/experimentiprimjer/:id" element={<Experimentiprimjer/>} />
+        <Route path="/experimenti/edit/:id" element={<EksperimentEdit />} />
+        <Route path="/komponenteprimjer/:id" element={<Komponenteprimjer/>} />
+        <Route path="/Experimentunos" element={<Experimentunos/>} />
+        <Route path="/Komponenteunos" element={<Komponenteunos/>} />
+        <Route path="/Komponente" element={<Komponente/>} />
+
+      </Routes>
+      </BrowserRouter>
+
+    </>
+  )
 }
 
-export default App;
+export default App
