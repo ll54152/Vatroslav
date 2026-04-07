306570235b0ecf82d4cb7602325af3b50894368d
Luka Lulić <ll54152@fer.hr>
2026-04-07 13:48:32 +0200

    Adjusted Home frontend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,39 +1,33 @@
 import '../Open.css'
-import { Link } from 'react-router-dom';
-import { Button } from '@/components/ui/button'
+import {Link} from 'react-router-dom';
+import {Button} from '@/components/ui/button'
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
+    Card,
+    CardContent,
+    CardHeader,
+    CardTitle,
 } from "@/components/ui/card"
 
 
 export default function Home() {
-  
-return(
-    
-      <Card>
 
-        <CardHeader>
-          <CardTitle>Baza Podataka</CardTitle>
-        </CardHeader>
+    return (
 
-        <CardContent className="font-mono">
-        <Link to="/login">
-          <Button className="m-5 bg-pink-500 text-white">Prijava</Button>
-          </Link>
+        <Card>
 
-          <Link to="/signup">
-          <Button className="m-5 bg-pink-500 text-white">Registracija</Button>
-          </Link>
-        </CardContent>
-        
-      </Card>
-      
-);
-  
+            <CardHeader>
+                <CardTitle>Inventar Vatroslav</CardTitle>
+            </CardHeader>
+
+            <CardContent className="font-mono">
+                <Link to="/login">
+                    <Button className="m-5 bg-pink-500 text-white">Prijava</Button>
+                </Link>
+            </CardContent>
+
+        </Card>
+
+    );
+
 }
 
