68b12bb958f0a7eea0f3ef408c45b27c1db18aca
Luka Lulić <ll54152@fer.hr>
2026-05-19 11:43:43 +0200

    Added link to Vatroslav in toolbar

--- frontend/src/components/Toolbar.jsx
+++ frontend/src/components/Toolbar.jsx
@@ -94,7 +94,9 @@ export default function Toolbar() {
             <Card className="mx-auto flex h-16 items-center justify-between px-4 md:px-6">
 
                 <div className="text-lg font-semibold text-pink-600">
-                    Vatroslav
+                    <Link className="text-lg font-semibold text-pink-600" to={isAuthenticated ? '/mainpage' : '/home'}>
+                        Vatroslav
+                    </Link>
                 </div>
 
                 <div className="hidden md:flex items-center gap-6">
